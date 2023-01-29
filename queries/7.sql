37a6e675eb1936ea9495d6f9593e111e86d96d05
Jordy <82671302+Jordy-6@users.noreply.github.com>
2023-01-29 23:11:43 +0100

    Insertions de données dans la table client

---
+++ queries/7.sql
@@ -0,0 +1,11 @@
+INSERT INTO myhotels.client
+(nom_client, prenom_client, tel_client, email, pays)
+VALUES('Johnson', 'Sara', '+44 673482634', 'sara@outlook.com', 'Anglettere'),
+('Diome', 'Fatou', '+223 721932822', 'datou@yahoo.fr', 'Mali'),
+('Nkosi', 'Mandela', '+27 46647464', 'mnk@yahoo.fr', 'Afrique du Sud'),
+('Madarame', 'Baku', '+81 12563337', 'stl@gmail.com', 'Japon'),
+('Buntaro', 'Mori', '+81 92369894', 'mori@outlook.com', 'Japon'),
+('Lavinho', 'Jr', '+55 76344782', 'bll@gmail.com', 'Bresil'),
+('Ri', 'Shin', '+86 46382942', '', 'Chine'),
+('Salamanca', 'Lalo', '+1 27838244', 'bcsl@gmail.com', 'Etats-Unis'),
+('Taylor', 'Lauren', '+1 73468963', 'lauren@outlook.com', 'Etats-Unis');

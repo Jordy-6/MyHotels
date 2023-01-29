41fa9c5c26d2f5ee36ed1168eecf36f1c8151a69
Jordy <82671302+Jordy-6@users.noreply.github.com>
2023-01-29 23:12:47 +0100

    Insertions de données dans la table prestataires

---
+++ queries/8.sql
@@ -0,0 +1,8 @@
+INSERT INTO myhotels.prestataires
+(nom_prestataire, taches_prestataire, nb_employes_prestataire, Tarif)
+VALUES('Clim-hotel', 'Maintenace de la climatisation', 7, 2500),
+('Reserv-hotel', 'Mise en place et maintenance système de gestion de réservation', 10, 25000),
+('Secur-hotel', 'Services de securités  tels que la surveillance video,la sécurité des clients etc...', 25, 20000),
+('Anim-hotel', 'Organisation de différentes activités : animations,divertissements,spectacles etc...', 40, 15000),
+('Mark-hotel', 'Services de marketing et de vente pour la promotion et optmisation de vos hotels', 30, 50000),
+('Rest-hotel', 'Services de restauration', 20, 30000);

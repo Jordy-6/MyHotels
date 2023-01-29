757b71a360ec7bc319bc01eb39f857af34eeb83c
Jordy <82671302+Jordy-6@users.noreply.github.com>
2023-01-29 23:14:38 +0100

    Insertions de données dans la table hotel

---
+++ queries/10.sql
@@ -0,0 +1,5 @@
+INSERT INTO myhotels.hotel
+(nom_hotel, adresse, ville, pays, email_hotel, numero_tel, nb_employes, nb_chambres, nb_reservations_actuelles, nb_clients_par_an, id_gerant_hotel, CA, depenses_annuelles)
+VALUES('Truth', '15 rue Ghost', 'Lyon', 'France', 'truthpro@gmail.com', '0109347433', 7, 15, 5, 10000, 24, 600000, 80000),
+('Fluph', '24 rue du time', 'Aubervilliers', 'France', 'fluph@gmail.com', '0132864877', 6, 9, 1, 3000, 15, 100000, 30000),
+('Tchuss', '36 avenue des eaux', 'Paris', 'France', 'tchusspro@gmail.com', '0145678556', 8, 4, 20, 20000, 22, 900000, 100000);

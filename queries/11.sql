691d3178baaffb757ad64c32c9972901166ded2a
Jordy <82671302+Jordy-6@users.noreply.github.com>
2023-01-29 23:15:44 +0100

    Insertions de données dans la table chambre

---
+++ queries/11.sql
@@ -0,0 +1,12 @@
+INSERT INTO myhotels.chambre
+(id_hotel_chambre, type_de_chambre, nb_lit, num_chambres, prix_chambres)
+VALUES(1, 'Chambre familiale', 4, 436, 50),
+(1, 'Chambre standard', 1, 12, 20),
+(1, 'Chambre de luxe', 2, 420, 100),
+(1, 'Chambre standard', 2, 32, 30),
+(1, 'Suite', 4, 1, 150),
+(2, 'Chambre familiale', 4, 13, 30),
+(3, 'Chambre standard', 1, 7, 30),
+(3, 'Chambre de luxe', 3, 23, 90),
+(3, 'Chambre standard', 1, 72, 45),
+(3, 'Chambre familiale', 4, 26, 60);
